1a22d3806374a8c5313496e61de1091ebb7a1b97
Prabin Nepal <prabinnepal1996@gmail.com>
2023-11-09 15:35:58 -0600

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import '../styles/globals.css';
+
+const RootLayout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient' />
+            </div>
+
+            <main className='app'>
+                {children}
+            </main>
+        </body>
+
+
+    </html>
+  )
+};
+
+export default RootLayout;
+
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">ChatFlowJs</span>
+        </h1>
+        <p className="desc text-center">Your Personal AI Assistant</p>
+      
+      {/* Feed */}
+      </section>
+  )
+}
+
+export default Home
